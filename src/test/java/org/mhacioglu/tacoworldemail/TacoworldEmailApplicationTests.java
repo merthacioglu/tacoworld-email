6adf0c0befe2c59815cca170562f2528eae22f08
Mert Hacioglu <mhacioglu96@gmail.com>
2024-09-23 20:56:39 +0100

    initial commit

---
+++ src/test/java/org/mhacioglu/tacoworldemail/TacoworldEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mhacioglu.tacoworldemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoworldEmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
